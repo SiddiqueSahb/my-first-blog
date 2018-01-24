0d9a6e2a36da458f56489d52ce0540ded6014d7a
asim776 <asimsiddiqui776@gmail.com>
2018-01-25 00:29:01 +0530

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qv^))#p9t&^u9k=1)rc%arjj!#$w1cx&gd5#pisq#3ax_a)*0('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'asim776.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'siddiqui776.pythonanywhere.com']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE =  'Asia/Kolkata'
 
 USE_I18N = True
 
