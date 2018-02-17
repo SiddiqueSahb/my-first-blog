2c7415ac702306afe9325e597b94726b18bb37dc
asim776 <asimsiddiqui776@gmail.com>
2018-02-17 17:59:37 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
+
+
+"""
+    ^ for the beginning of the text
+    $ for the end of the text
+    \d for a digit
+    + to indicate that the previous item should be repeated at least once
+    () to capture part of the pattern
+    post/ just means that after the beginning
+"""
